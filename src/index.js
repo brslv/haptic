3d14c35403cd88464fc713eae3cd754241221a55
Borislav Grigorov <bobi@noblehire.io>
2021-02-15 11:31:16 +0200

    use redis url to connect

--- src/index.js
+++ src/index.js
@@ -54,8 +54,7 @@ app.use(
 );
 const RedisStore = connectRedis(expressSession);
 const redisClient = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
+  url: process.env.REDIS_URL,
 });
 app.use(
   expressSession({
