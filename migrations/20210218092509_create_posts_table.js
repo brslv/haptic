e2069a462cc72f3680e78f17e3bac04d5d16f1ca
Borislav Grigorov <bobi@noblehire.io>
2021-02-18 14:21:14 +0200

    add post type buttons to the head of the dashboard posts page

---
+++ migrations/20210218092509_create_posts_table.js
@@ -0,0 +1,34 @@
+const knex = require("knex");
+
+exports.up = function(knex) {
+  return knex.schema.hasTable("posts").then(function(exists) {
+    if (!exists) {
+      return knex.schema.createTable("posts", function(t) {
+        t.increments("id").primary();
+        t.integer("product_id")
+          .unsigned()
+          .notNullable();
+        t.foreign("product_id")
+          .references("id")
+          .inTable("products")
+          .onDelete("CASCADE");
+        t.integer("user_id")
+          .unsigned()
+          .notNullable();
+        t.foreign("user_id")
+          .references("id")
+          .inTable("users")
+          .onDelete("CASCADE");
+        t.text("text").notNullable();
+        t.timestamp("created_at")
+          .notNullable()
+          .defaultTo(knex.fn.now());
+        t.timestamp("updated_at").nullable();
+      });
+    }
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable("posts");
+};
