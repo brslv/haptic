4a0057bbb5cbeb8f5931dcb168f4b0728e102844
Borislav Grigorov <bobi@noblehire.io>
2021-02-18 19:35:18 +0200

    posts page interactions with post types buttons

--- public/js/posts.js
+++ public/js/posts.js
@@ -1,4 +1,75 @@
 document.addEventListener("DOMContentLoaded", function domLoaded() {
+  // handling btn types clicks
+  var btnSelectors = {
+    text: "[data-bind=text]",
+    youtube: "[data-bind=youtube]",
+    survey: "[data-bind=survey]",
+    milestone: "[data-bind=milestone]",
+    roadmap: "[data-bind=roadmap]",
+  };
+  var btns = Object.values(btnSelectors).map(function(sel) {
+    return document.querySelector(sel);
+  });
+
+  function handleCloseBtnClick(e) {
+    e.stopPropagation();
+    var form = document.querySelector(
+      '[data-show="' + e.currentTarget.dataset.close + '"]'
+    );
+    if (form) {
+      form.classList.add("hidden");
+    }
+    // open all btns
+    [].forEach.call(btns, function(btn) {
+      var closeBtn = btn.querySelector("[data-close]");
+      btn.classList.remove("hidden");
+      if (closeBtn) closeBtn.classList.add("hidden");
+    });
+  }
+
+  [].forEach.call(btns, function(btn) {
+    btn.addEventListener("click", function btnClick(e) {
+      if (e.currentTarget.dataset.disabled !== undefined) {
+        // the button is disabled, do not open it.
+        return;
+      }
+
+      // 1. hide the other buttons and their bindings.
+      [].forEach.call(btns, function hideBtn(_btn) {
+        var closeBtn = _btn.querySelector("[data-close]");
+        if (closeBtn) {
+          closeBtn.addEventListener("click", handleCloseBtnClick);
+        }
+
+        if (e.currentTarget.isEqualNode(_btn)) {
+          if (closeBtn) closeBtn.classList.remove("hidden");
+          return;
+        }
+
+        if (closeBtn) {
+          closeBtn.classList.add("hidden");
+          closeBtn.removeEventListener("click", handleCloseBtnClick);
+        }
+        _btn.classList.add("hidden");
+      });
+
+      // 2. show the form that's bound to the button
+      var form = document.querySelector(
+        '[data-show="' + e.currentTarget.dataset.bind + '"]'
+      );
+      if (form) {
+        form.classList.remove("hidden");
+      } else {
+        console.error(
+          "Couldn't find the proper form, bound to " +
+            e.currentTarget.dataset.bind +
+            " btn."
+        );
+      }
+    });
+  });
+
+  // handling text form submit
   var formPostTextEl = document.getElementById("form-post-text");
   var postsContainer = document.getElementById("posts-container");
   var noPostsMsg = document.getElementById("no-posts-msg");
