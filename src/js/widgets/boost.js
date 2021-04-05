3f1b80bf8b41d3614c14a94fd42bdbfce15322ec
Borislav Grigorov <bobi@noblehire.io>
2021-04-05 17:27:23 +0300

    fe scripts: handle product boosts

--- src/js/widgets/boost.js
+++ src/js/widgets/boost.js
@@ -25,6 +25,35 @@ export default function boost() {
     req(`/post/${postId}/boost`, { method: "post", ok: onOk, fail: onFail });
   }
 
+  function onProductBoostClick($el, $els) {
+    const $counter = $("[data-product-boost-counter]");
+    const $slug = $("[data-product-slug]");
+    const $boostsCounter = $("[data-product-boosts-count]");
+    const slug = $slug.data("product-slug");
+    const boostsCount = $boostsCounter.data("product-boosts-count");
+    if (!slug) return;
+
+    function onOk(response) {
+      const newCount = isNaN(boostsCount) ? 0 : Number(boostsCount) + 1;
+      $counter.text(newCount);
+      $boostsCounter.data("product-boosts-count", newCount);
+    }
+
+    function onFail(err) {
+      const data = err.response.data;
+      $(document).trigger("haptic:add-toast", {
+        content: data.err,
+        type: "error",
+      });
+    }
+
+    req(`/p/${slug}/boost`, {
+      method: "post",
+      ok: onOk,
+      fail: onFail,
+    });
+  }
+
   function load($els) {
     $els.$postBoostBtns.on("click", function(e) {
       e.preventDefault();
@@ -32,12 +61,19 @@ export default function boost() {
       if ($this.data("disabled") !== undefined) return;
       onPostBoostClick($this, $els);
     });
+    $els.$productBoostBtn.on("click", function(e) {
+      e.preventDefault();
+      const $this = $(this);
+      if ($this.data("disabled") !== undefined) return;
+      onProductBoostClick($this, $els);
+    });
   }
 
   let $els = {};
   turbo.load(() => {
     $els = {
       $postBoostBtns: $("[data-post-boost-btn]"),
+      $productBoostBtn: $("[data-product-boost-btn]"),
     };
 
     load($els);
