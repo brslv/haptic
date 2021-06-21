60803db14a615899286de0d089c0eab17ac376bb
Borislav Grigorov <bobi@noblehire.io>
2021-06-21 15:24:41 +0300

    fix: comments textarea don't autoresize in browse page

--- src/js/widgets/textarea-autoresize.js
+++ src/js/widgets/textarea-autoresize.js
@@ -1,7 +1,7 @@
-import { turbo } from "../utils";
+import { turbo, onFrameLoaded } from "../utils";
 
 export default function textareaAutoresize() {
-  turbo.load(() => {
+  function load() {
     document.querySelectorAll("[data-autoresize]").forEach(function(element) {
       element.style.boxSizing = "border-box";
       var offset = element.offsetHeight - element.clientHeight;
@@ -11,5 +11,10 @@ export default function textareaAutoresize() {
       });
       element.removeAttribute("data-autoresize");
     });
+  }
+
+  turbo.load(() => {
+    load();
+    onFrameLoaded("browse-posts-list", load);
   });
 }
